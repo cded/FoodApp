029003050b698683cb7328f5e989ce1704e55e80
Youssef <youssef.akallal@gmail.com>
2015-10-16 18:36:24 -0400

    changes

--- assignment/src/assignment1.java
+++ assignment/src/assignment1.java
@@ -1,14 +1,412 @@
-import java.util.Scanner;
+// -----------------------------------------------------
+// Assignment 2
+// Written by: Youssef Akallal 260447651
+// For CCCS 300
+// -----------------------------------------------------
 
+
+import javax.swing.JOptionPane; 
+//This program will determine 2 output with 2 simple operations on an input entered by the user.
 public class assignment1 {
 public static void main(String[] args){
-	System.out.println("Welcome to the candy store!");
-	Scanner user_input = new Scanner(System.in);
-	System.out.println("How many coupons do you have?");
-	int coupons = user_input.next();
-	int candyBars = coupons/5;
-	int freeGames = (coupons%5)/3;
-	System.out.println("You have" + candyBars + "candy bars and" + freegames "freeGames");
-	System.out.println("Enjoy!");
+	double commissionYear=0;
+	while (commissionYear < 100000.0){
+	System.out.println("Welcome to January");
+	double commissionMonth1=0;
+	while(commissionMonth1<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
 	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth1+=commission1;
+	}
+	commissionYear+=commissionMonth1;
+	System.out.println("Your commission pay for this month is " + commissionMonth1);
+
+	//February
+	System.out.println("Welcome to February");
+	double commissionMonth2=0;
+	while(commissionMonth2<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth2+=commission1;
+	}
+	commissionYear+=commissionMonth2;
+	System.out.println("Your commission pay for this month is " + commissionMonth2);
+
+	//March
+	System.out.println("Welcome to March");
+	double commissionMonth3=0;
+	while(commissionMonth3<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth3+=commission1;
+	}
+	commissionYear+=commissionMonth3;
+	System.out.println("Your commission pay for this month is " + commissionMonth3);
+
+	//April
+	System.out.println("Welcome to April");
+	double commissionMonth4=0;
+	while(commissionMonth4<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth4+=commission1;
+	}
+	commissionYear+=commissionMonth4;
+	System.out.println("Your commission pay for this month is " + commissionMonth4);
+
+	//May
+	System.out.println("Welcome to May");
+	double commissionMonth5=0;
+	while(commissionMonth5<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth5+=commission1;
+	}
+	commissionYear+=commissionMonth5;
+	System.out.println("Your commission pay for this month is " + commissionMonth5);
+
+	//June
+	System.out.println("Welcome to June");
+	double commissionMonth6=0;
+	while(commissionMonth6<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth6+=commission1;
+	}
+	commissionYear+=commissionMonth6;
+	System.out.println("Your commission pay for this month is " + commissionMonth6);
+
+	//july
+	System.out.println("Welcome to July");
+	double commissionMonth7=0;
+	while(commissionMonth7<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth7+=commission1;
+	}
+	commissionYear+=commissionMonth7;
+	System.out.println("Your commission pay for this month is " + commissionMonth7);
+
+	//August
+	System.out.println("Welcome to August");
+	double commissionMonth8=0;
+	while(commissionMonth8<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth8+=commission1;
+	}
+	commissionYear+=commissionMonth8;
+	System.out.println("Your commission pay for this month is " + commissionMonth8);
+
+	//September
+	System.out.println("Welcome to September");
+	double commissionMonth9=0;
+	while(commissionMonth9<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth9+=commission1;
+	}
+	commissionYear+=commissionMonth9;
+	System.out.println("Your commission pay for this month is " + commissionMonth9);
+
+	//October
+	System.out.println("Welcome to October");
+	double commissionMonth10=0;
+	while(commissionMonth10<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth10+=commission1;
+	}
+	commissionYear+=commissionMonth10;
+	System.out.println("Your commission pay for this month is " + commissionMonth10);
+
+	//November
+	System.out.println("Welcome to November");
+	double commissionMonth11=0;
+	while(commissionMonth11<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth11+=commission1;
+	}
+	commissionYear+=commissionMonth11;
+	System.out.println("Your commission pay for this month is " + commissionMonth11);
+
+	//December
+	System.out.println("Welcome to December");
+	double commissionMonth12=0;
+	while(commissionMonth12<15000){
+	int option = JOptionPane.showConfirmDialog(null,"Do we have a customer?", "Select an option", JOptionPane.YES_NO_OPTION);
+	double transaction=Math.random()*50000.0;
+	transaction=Math.round(transaction*100)/100;
+	if(option==JOptionPane.YES_OPTION){
+		System.out.println("Welcome to the ABC store");
+		int optionTransaction = JOptionPane.showConfirmDialog(null,"Those items cost " + transaction + "$ Do you buy?", "Select an option", JOptionPane.YES_NO_OPTION);
+		if (optionTransaction==JOptionPane.YES_OPTION){
+			System.out.println("Thanks for the " + transaction + "$ transaction");
+		}
+	}
+	else if (option == JOptionPane.NO_OPTION){
+		System.out.println("OK, see you next time.");
+	}
+	double commission1=0;
+	if (transaction<10000 && transaction>0.01){
+		commission1=transaction*0.1;
+	}
+	else if (transaction<30000 && transaction>10000.01){
+		commission1=10000*0.1+(transaction-10000)*0.15;
+	}
+	else if (transaction>30000.01){
+		commission1=10000*0.1+20000*0.15+(transaction-30000)*0.2;
+	}
+	commissionMonth12+=commission1;
+	}
+	commissionYear+=commissionMonth12;
+	System.out.println("Your commission pay for this month is " + commissionMonth12);
+
+}
+	System.out.println("Your earnings for this year = " + commissionYear + "$");
+	System.out.println("Welcome to Honolulu! Enjoy the rest of this year!");
+
+
+	/*Total
+	double commissionYear= commissionMonth1+commissionMonth2+commissionMonth3+commissionMonth4+commissionMonth5+commissionMonth6+commissionMonth7+commissionMonth8+commissionMonth9+commissionMonth10+commissionMonth11+commissionMonth12;
+	System.out.println("Your earnings for this year = " + commissionYear + "$");
+	if (commissionYear > 100000.00){
+		System.out.println("Welcome to Honolulu! Enjoy the rest of this year!");
+	}
+	else{
+		System.out.println("Enjoy Montreal!");
+	}*/
+}
 }
--- zest/www/js/script.js
+++ zest/www/js/script.js
@@ -10,14 +10,16 @@ orderNow();
 counter('#itemcounter1');
 counter('#itemcounter2');
 counter('#itemcounter3');
+swipeTest();
+processingRoutine();
 };
 
 function menuSlide() {
   $('.homeIcon').click(
     function() {
-      $(".menuCalled").show();
-      $(".menuContainer").show();
-      $('.bottom').hide();
+      $(".menuCalled").show("slide",{ direction: "left" }, 400);
+      $(".menuContainer").show("slide",{ direction: "left" }, 400);
+      $('.orderNow').hide();
       $(".top").hide();
       $(".homeIcon").hide();
       $('body').fade();
@@ -25,37 +27,91 @@ function menuSlide() {
     });
 
   $('.menuOutside').click(function() {
-    $(".menuCalled").hide();
-    $(".menuContainer").show();
-    $('.bottom').show();
+    $(".menuCalled").hide("slide",{ direction: "left" }, 400);
+    $(".menuContainer").hide();
+    $('.orderNow').show();
     $(".top").show();
     $(".homeIcon").show();
   });
   $('.menuContainer').click(function(event){
       event.stopPropagation();
       if($(event.target).is('#profile')){
-        $('.profileContainer').show();
+        $('.profileContainer').slideDown();
         $(".menuCalled").hide();
         $(".menuContainer").show();
         $(".homeIcon").show();
       };
        if($(event.target).is('#todayMenu')){
         $('.menuCalled').hide();
-        $('.bottom').show();
+        $('.orderNow').show();
         $('.top').show();
         $(".homeIcon").show();
       };
+      if($(event.target).is('#share')){
+        $('.social').slideDown();
+        $(".menuCalled").hide();
+        $(".menuContainer").show();
+        $(".homeIcon").show();
+      };
+      if($(event.target).is('#payment')){
+        $('.containerPayment').slideDown();
+        $(".menuCalled").hide();
+        $(".menuContainer").show();
+        $(".homeIcon").show();
+      };
+      if($(event.target).is('#notifications')){
+        $('.containerNotifications').slideDown();
+        $(".menuCalled").hide();
+        $(".menuContainer").show();
+        $(".homeIcon").show();
+      };
+      if($(event.target).is('#about')){
+        $('.containerAbout').slideDown();
+        $(".menuCalled").hide();
+        $(".menuContainer").show();
+        $(".homeIcon").show();
+      };
 
-  $('.closeProfile').click(function(){
-    $('.profileContainer').hide();
-    $('.bottom').show();
+  $('#closeProfile').click(function(){
+    $('.profileContainer').slideUp();
+    $('.orderNow').show();
     $('.top').show();
   });
-    $('.icon-close').click(function(){
-        $('.menuCalled').hide();
-        $('.bottom').show();
-        $('.top').show();
-        $(".homeIcon").show();
+  $('.icon-close').click(function(){
+    $('.menuCalled').hide();
+    $('.orderNow').show();
+    $('.top').show();
+    $(".homeIcon").show();
+  });
+  $('#closeShare').click(function(){
+    $('.social').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
+  });
+  $('body').click(function(){
+    $('.social').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
+  });
+  $('homeIcon').click(function(){
+    $('.social').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
+  });
+  $('#closePayment').click(function(){
+    $('.containerPayment').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
+  });
+  $('#closeNotifications').click(function(){
+    $('.containerNotifications').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
+  });
+  $('#closeAbout').click(function(){
+    $('.containerAbout').slideUp();
+    $('.orderNow').show();
+    $('.top').show();
   });
   /*$('.profileContainer').click(function(event)){
     event.stopPropagation();
@@ -110,24 +166,24 @@ function learnMore() {
 };
 
 function orderNow(){
-    $('.bottom').click(function(){
-      $('.container').show();
-      $('.bottom').hide();
+    $('.orderNow').click(function(){
+      $('.containerLogin').slideDown();
+      $('.orderNow').hide();
     });
 
     $('#closeLogin').click(function(){
-      $('.container').hide();
-      $('.bottom').show();
+      $('.containerLogin').slideUp();
+      $('.orderNow').show();
     });
     
     $('.signUpHere').click(function(){
-      $('.container').hide();
+      $('.containerLogin').hide();
       $('.profileContainer').show();
     });
-      $('.closeProfile').click(function(){
-    $('.profileContainer').hide();
-    $('.bottom').show();
-    $('.top').show();
+    $('#closeProfile').click(function(){
+      $('.profileContainer').slideUp();
+      $('.orderNow').show();
+      $('.top').show();
   });
 };
 
@@ -136,7 +192,45 @@ function stopProp(a){
     event.stopPropagation();
   });
 };
+
+
+/*function swipeTest(){
+  $("body").swipe({
+  swipeRight:function(event, direction, distance, duration, fingerCount) {
+    $(".menuCalled").show("slide",{ direction: "left" }, 400);
+      $(".menuContainer").show("slide",{ direction: "left" }, 400);
+      $(".homeIcon").hide();
+      $('body').fade();  
+    },
+    swipeLeft:function(event, direction, distance, duration, fingerCount){
+      $(".menuCalled").hide("slide",{ direction: "left" }, 400);
+      $(".menuContainer").hide();
+      $(".homeIcon").show();
+    }
+  });
+};*/
 /*$('#pagepiling').pagepiling({
     anchors: ['#page1', '#page2', '#page3'],
     menu: '#myMenu'
 });*/
+function processingRoutine() {
+  var swipedElement = document.getElementById(triggerElementID);
+  if ( swipeDirection == 'left' ) {
+    $(".menuCalled").hide("slide",{ direction: "left" }, 400);
+    $(".menuContainer").hide();
+    $('.orderNow').show();
+    $(".top").show();
+    $(".homeIcon").show();
+  } else if ( swipeDirection == 'right' ) {
+      $(".menuCalled").show("slide",{ direction: "left" }, 400);
+      $(".menuContainer").show("slide",{ direction: "left" }, 400);
+      $('.orderNow').hide();
+      $(".top").hide();
+      $(".homeIcon").hide();
+      $('body').fade();
+  } else if ( swipeDirection == 'up' ) {
+
+  } else if ( swipeDirection == 'down' ) {
+
+  }
+}
